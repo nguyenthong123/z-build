09636a7cd8222e6a17898e3b08756514e81c0f50
nguyenthong123 <nbt1024@gmail.com>
2026-03-31 08:46:33 +0700

    fix: allow firebase-config.js in git for service worker

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+// File này được tự động sinh ra từ .env - KHÔNG NÊN commit lên GitHub
+const firebaseConfig = {
+  apiKey: "AIzaSyDFDyDOZlplNltgcYA3VydZT0WA4ogOIMo",
+  authDomain: "z-build-dunvex.firebaseapp.com",
+  projectId: "z-build-dunvex",
+  storageBucket: "z-build-dunvex.firebasestorage.app",
+  messagingSenderId: "1057831056165",
+  appId: "1:1057831056165:web:60f13cee942d02ba7220cc"
+};
