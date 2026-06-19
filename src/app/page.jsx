504fcbcd1e1ed0899da61548253e53e09b2304d4
nguyenthong123 <nbt1024@gmail.com>
2026-06-19 07:18:16 +0700

    fix: add all providers to page.jsx (BrowserRouter, AuthProvider, StoreProvider, etc)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n/config';
+import { ToastProvider } from '../context/ToastContext';
+import { ThemeProvider } from '../context/ThemeContext';
+import { WishlistProvider } from '../context/WishlistContext';
+import { AdminAIProvider } from '../context/AdminAIContext';
+import { AuthProvider } from '../context/AuthContext';
+import { StoreProvider } from '../context/StoreContext';
 
-// Dynamic import to avoid SSR issues with Firebase and browser APIs
 const App = dynamic(() => import('../App'), {
   ssr: false,
   loading: () => (
@@ -33,7 +40,23 @@ const App = dynamic(() => import('../App'), {
 export default function Page() {
   return (
     <I18nextProvider i18n={i18n}>
-      <App />
+      <HelmetProvider>
+        <BrowserRouter>
+          <ThemeProvider>
+            <AuthProvider>
+              <StoreProvider>
+                <WishlistProvider>
+                  <ToastProvider>
+                    <AdminAIProvider>
+                      <App />
+                    </AdminAIProvider>
+                  </ToastProvider>
+                </WishlistProvider>
+              </StoreProvider>
+            </AuthProvider>
+          </ThemeProvider>
+        </BrowserRouter>
+      </HelmetProvider>
     </I18nextProvider>
   );
 }
