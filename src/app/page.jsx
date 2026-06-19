893771d2f5d99eec82896889090163bfbcaf7eec
nguyenthong123 <nbt1024@gmail.com>
2026-06-19 07:18:37 +0700

    fix: ClientApp wrapper with all providers (BrowserRouter etc) for static export

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,9 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n/config';
-import { ToastProvider } from '../context/ToastContext';
-import { ThemeProvider } from '../context/ThemeContext';
-import { WishlistProvider } from '../context/WishlistContext';
-import { AdminAIProvider } from '../context/AdminAIContext';
-import { AuthProvider } from '../context/AuthContext';
-import { StoreProvider } from '../context/StoreContext';
 
-const App = dynamic(() => import('../App'), {
+// All providers + App must be client-side only (BrowserRouter needs document)
+const ClientApp = dynamic(() => import('./ClientApp'), {
   ssr: false,
   loading: () => (
     <div style={{
@@ -38,25 +29,5 @@ const App = dynamic(() => import('../App'), {
 });
 
 export default function Page() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <HelmetProvider>
-        <BrowserRouter>
-          <ThemeProvider>
-            <AuthProvider>
-              <StoreProvider>
-                <WishlistProvider>
-                  <ToastProvider>
-                    <AdminAIProvider>
-                      <App />
-                    </AdminAIProvider>
-                  </ToastProvider>
-                </WishlistProvider>
-              </StoreProvider>
-            </AuthProvider>
-          </ThemeProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </I18nextProvider>
-  );
+  return <ClientApp />;
 }
